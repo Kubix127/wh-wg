64e28dad8ec7d24abe4b5c580c18db41205d45f3
Kubix127 <jakubsad01@gmail.com>
2022-05-05 21:27:53 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '.../build', 'index.html'));
+        res.sendFile(path.resolve('build', 'index.html'));
     })
 }
 
